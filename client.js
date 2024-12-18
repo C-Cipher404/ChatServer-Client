e87d6c83122e88d53cd6565a69c257453d096210
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2024-12-18 08:55:45 -0700

    adding

--- client.js
+++ client.js
@@ -1,10 +1,24 @@
 const net = require("net");
+const readline = require("readline");
 
 const client = net.createConnection(5000, () => {
-  console.log("Connected");
+  console.log("Connected to the server.");
 });
 
-client.setEncoding("utf8");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 client.on("data", (data) => {
-  console.log(data);
+  console.log(data.toString());
+});
+
+rl.on("line", (input) => {
+  client.write(input);
+});
+
+client.on("end", () => {
+  console.log("Disconnected from the server.");
+  rl.close();
 });
--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const net = require("net");
+const fs = require("fs");
 
-const server = net
-  .createServer((client) => {
-    client.write("Welcome to the chat room");
-  })
-  .listen(5000, () => {
-    console.log("Listening on port 5000");
+let clientId = 0;
+let clients = [];
+
+const server = net.createServer((client) => {
+  clientId++;
+  client.name = `Client${clientId}`;
+
+  fs.appendFile("chat.log", `${client.name} connected\n`, (err) => {
+    if (err) throw err;
   });
+
+  client.write(`Welcome to the chat room, ${client.name}!\n`);
+
+  clients.forEach((otherClient) => {
+    if (otherClient !== client) {
+      otherClient.write(`${client.name} has joined the chat.\n`);
+    }
+  });
+
+  clients.push(client);
+
+  client.on("data", (data) => {
+    const message = `${client.name}: ${data}`;
+
+    clients.forEach((otherClient) => {
+      if (otherClient !== client) {
+        otherClient.write(message);
+      }
+    });
+
+    fs.appendFile("chat.log", message, (err) => {
+      if (err) throw err;
+    });
+  });
+
+  client.on("end", () => {
+    clients = clients.filter((otherClient) => otherClient !== client);
+    clients.forEach((otherClient) => {
+      otherClient.write(`${client.name} has left the chat.\n`);
+    });
+
+    fs.appendFile("chat.log", `${client.name} disconnected\n`, (err) => {
+      if (err) throw err;
+    });
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server listening on port 5000...");
+});
