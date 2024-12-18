d0242d64765baf3ae48049a40a2a332724d6a67f
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2024-12-18 08:45:28 -0700

    test

--- client.js
+++ client.js
@@ -3,3 +3,8 @@ const net = require("net");
 const client = net.createConnection(5000, () => {
   console.log("Connected");
 });
+
+client.setEncoding("utf8");
+client.on("data", (data) => {
+  console.log(data);
+});
