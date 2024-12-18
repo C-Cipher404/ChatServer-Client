bf976da63e381f934645b8b8bfec0b3c90461147
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2024-12-18 08:34:16 -0700

    test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const net = require("net");
 
 const server = net
-  .createServer((client) => {})
+  .createServer((client) => {
+    client.write("Welcome to the chat room");
+  })
   .listen(5000, () => {
     console.log("Listening on port 5000");
   });
